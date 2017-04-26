1cc42e35c08045bcee85177846a93bdc98807b6d
todor-enikov <enigmaenikov@gmail.com>
2017-04-26 20:40:18 +0300

    Finished model for the database.

--- Source/InventoryManager.Data.Models/Clothes.cs
+++ Source/InventoryManager.Data.Models/Clothes.cs
@@ -12,18 +12,31 @@ namespace InventoryManager.Data.Models
         [Key]
         public Guid Id { get; set; }
 
+        [Required]
+        [MinLength(5, ErrorMessage = "The minimum length is 5 symbols!")]
+        [MaxLength(100, ErrorMessage = "The maximum length is 100 symbols!")]
         public string Name { get; set; }
 
+        [Required]
         public ClothesType Type { get; set; }
 
+        [Required]
+        [Range(1, 10, ErrorMessage = "The quantity should be between 1 and 10 quantities!")]
         public double Quantity { get; set; }
 
+        [Required]
         public SizeType Size { get; set; }
 
+        [Required]
+        [Range(0.01, 1000, ErrorMessage = "The price should be between 0.01 and 1000 $!")]
         public decimal Price { get; set; }
 
+        [Required]
         public string ImagePath { get; set; }
 
+        [Required]
+        [MinLength(5, ErrorMessage = "The minimum length is 5 symbols!")]
+        [MaxLength(1000, ErrorMessage = "The maximum length is 1000 symbols!")]
         public string Description { get; set; }
 
         public string UserId { get; set; }
--- Source/InventoryManager.Data.Models/ClothesType.cs
+++ Source/InventoryManager.Data.Models/ClothesType.cs
@@ -8,7 +8,15 @@ namespace InventoryManager.Data.Models
 {
     public enum ClothesType
     {
-        Tshirt=0,
-
+        Tshirt = 0,
+        Shirt = 1,
+        Jeans = 2,
+        Coat = 3,
+        Shorts = 4,
+        Jacket = 5,
+        Skirt = 6,
+        Dress = 7,
+        Suit = 8,
+        Socks = 9
     }
 }

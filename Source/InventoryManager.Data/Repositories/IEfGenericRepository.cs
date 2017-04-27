1f0f0fb54b03bc24495ef06aea77d5258fe3a1be
todor-enikov <enigmaenikov@gmail.com>
2017-04-27 18:36:03 +0300

    Added basic repositories for the project.

---
+++ Source/InventoryManager.Data/Repositories/IEfGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Data.Repositories
+{
+    public interface IEfGenericRepository<T>
+          where T : class
+    {
+        IQueryable<T> All();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Delete(object id);
+
+        int SaveChanges();
+    }
+}

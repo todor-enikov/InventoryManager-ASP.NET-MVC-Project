5127f1cb97f0402e2f8441a9ce23562d6542f9e8
todor-enikov <enigmaenikov@gmail.com>
2017-04-28 10:57:37 +0300

    Created two models: UserClothesViewModel and UserDetailsViewModel.

---
+++ Source/InventoryManager.Client.MVC/Models/UserViewModels/UserClothesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManager.Client.MVC.Models.UserViewModels
+{
+    public class UserClothesViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Source/InventoryManager.Client.MVC/Models/UserViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManager.Client.MVC.Models.UserViewModels
+{
+    public class UserDetailsViewModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public List<UserClothesViewModel> Clothes { get; set; }
+    }
+}

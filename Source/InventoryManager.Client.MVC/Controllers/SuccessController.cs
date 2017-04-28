3fb73fb086c33fb126762613912452047509ef19
todor-enikov <enigmaenikov@gmail.com>
2017-04-28 11:23:10 +0300

    Added success controller and view for it.

---
+++ Source/InventoryManager.Client.MVC/Controllers/SuccessController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManager.Client.MVC.Controllers
+{
+    public class SuccessController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string submit)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}

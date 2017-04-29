0e669a0b23eb3aeaf2b638040663228e3e3c43f3
todor-enikov <enigmaenikov@gmail.com>
2017-04-29 22:06:56 +0300

    Added UserAttributes unit tests.

---
+++ Source/InventoryManager.UnitTests/InventoryManager.Data.Models/UserAttributes.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using NUnit.Framework;
+using InventoryManager.Data.Models;
+using System.Linq;
+using Castle.Components.DictionaryAdapter;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryManager.UnitTests.InventoryManager.Data.Models
+{
+    [TestFixture]
+    public class UserAttributes
+    {
+        [Test]
+        public void Email_ShouldHaveAttributeEmailAddress()
+        {
+            // Arrange
+            var user = new User();
+            string property = "Email";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(EmailAddressAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void UserName_ShouldHaveAttributeIndex()
+        {
+            // Arrange
+            var user = new User();
+            string property = "UserName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(IndexAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void FirstName_ShouldHaveAttributeRequired()
+        {
+            // Arrange
+            var user = new User();
+            string property = "FirstName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(RequiredAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void FirstName_ShouldHaveAttributeMinLength()
+        {
+            // Arrange
+            var user = new User();
+            string property = "FirstName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(MinLengthAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void FirstName_ShouldHaveAttributeMaxLength()
+        {
+            // Arrange
+            var user = new User();
+            string property = "FirstName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(MaxLengthAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void LastName_ShouldHaveAttributeRequired()
+        {
+            // Arrange
+            var user = new User();
+            string property = "LastName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(RequiredAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void LastName_ShouldHaveAttributeMinLength()
+        {
+            // Arrange
+            var user = new User();
+            string property = "LastName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(MinLengthAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+
+        [Test]
+        public void LastName_ShouldHaveAttributeMaxLength()
+        {
+            // Arrange
+            var user = new User();
+            string property = "LastName";
+
+            // Act
+            bool hasAttribute = user.GetType()
+                                    .GetProperty(property)
+                                    .GetCustomAttributes(false)
+                                    .Where(p => p.GetType() == typeof(MaxLengthAttribute))
+                                    .Any();
+
+            // Assert
+            Assert.IsTrue(hasAttribute);
+        }
+    }
+}

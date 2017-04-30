2c5ef2d47b204b917ca4957c4a3e28cdfaa420d3
todor-enikov <enigmaenikov@gmail.com>
2017-04-30 15:50:03 +0300

    Added InventoryManagerDbContext unit tests.

---
+++ Source/InventoryManager.UnitTests/InventoryManager.Data/InventoryManagerDbContext_Should.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using InventoryManager.Data;
+
+namespace InventoryManager.UnitTests.InventoryManager.Data
+{
+    [TestFixture]
+    public class InventoryManagerDbContext_Should
+    {
+        [Test]
+        public void CreateInstanceOfDatabaseContext()
+        {
+            var context = new InventoryManagerDbContext();
+
+            Assert.IsInstanceOf<InventoryManagerDbContext>(context);
+        }
+
+        [Test]
+        public void CreateInstanceOfIInventoryManagerDbContext()
+        {
+            var context = new InventoryManagerDbContext();
+
+            Assert.IsInstanceOf<IInventoryManagerDbContext>(context);
+        }
+
+        [Test]
+        public void MethodCreateInstanceOfInventoryManagerDbContext()
+        {
+            var context = InventoryManagerDbContext.Create();
+
+            Assert.IsInstanceOf<InventoryManagerDbContext>(context);
+        }
+
+        [Test]
+        public void MethodCreateInstanceOfIInventoryManagerDbContext()
+        {
+            var context =  InventoryManagerDbContext.Create();
+            
+            Assert.IsInstanceOf<IInventoryManagerDbContext>(context);
+        }
+    }
+}

69ce010bea565857a80adab914d906a99d402b06
todor-enikov <enigmaenikov@gmail.com>
2017-04-30 20:10:18 +0300

    Added ErrorController.

---
+++ Source/InventoryManager.Client.MVC/Controllers/ErrorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManager.Client.MVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        public ActionResult NotFound()
+        {
+            return View("NotFound");
+        }
+
+        [HttpGet]
+        public ActionResult Forbidden()
+        {
+            return View("Forbidden");
+        }
+
+        [HttpGet]
+        public ActionResult InternalServer()
+        {
+            return View("InternalServer");
+        }
+
+        [HttpGet]
+        public ActionResult UnAuthorized()
+        {
+            return View("UnAuthorized");
+        }
+
+        [HttpGet]
+        public ActionResult BadRequest()
+        {
+            return View("BadRequest");
+        }
+    }
+}
